62a9aee10ffc96581cd3351e39d2f70f4bb48f8c
carla <carla@example.com>
2023-06-16 12:37:29 +0300

    update js

--- index.js
+++ index.js
@@ -61,9 +61,24 @@ secondProject.addEventListener("mouseout", (e) => {
     secondProjectHover.classList.add("hidden");
 })
 
+//  SCROLL SECTIONS ACTIVE LINK
+const sections = document.querySelectorAll('section[id]');
 
+function scrollActive(){
+    const scrollY = window.pageYOffset;
 
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id');
 
-
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link');
+        }else{
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        };
+    })
+}
+window.addEventListener('scroll', scrollActive);
 
 
